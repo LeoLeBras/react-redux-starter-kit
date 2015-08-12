2065cf2b3eb36beddf6c591859ba7ca3e3586aac
Le Bras <leo.lebrasf@gmail.com>
2015-08-12 15:52:39 +0200

    Beautify reducers

--- src/scripts/reducers/samples.jsx
+++ src/scripts/reducers/samples.jsx
@@ -1,12 +1,8 @@
 import { handleActions } from 'redux-actions';
-import {
-    SAMPLE
-} from 'constants/ActionTypes';
+import { SAMPLE } from 'constants/ActionTypes';
 
-const samples = handleActions({
+export const samples = handleActions({
     [SAMPLE]: (state) => (
         state
     )
 }, []);
-
-export default samples;
