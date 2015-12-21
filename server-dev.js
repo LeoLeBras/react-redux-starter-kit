cfbb4d997a7a093557ae22d2eea430f0e50a50d3
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-12-21 17:23:52 +0100

    Fix server-dev.js

---
+++ server-dev.js
@@ -0,0 +1,26 @@
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import config from './webpack.config.babel';
+
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(config.server.port, 'localhost', function (err) {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log('Listening at http://localhost:' + config.server.port);
+});
